414fe02baaa9b96477526f75268b75f389bc0da0
Aveskulov <WenJoymk3@gmail.com>
2023-07-18 10:32:41 +0200

    create second file

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('node:fs/promises');
+const firstFile = "./first.txt";
+const secondFile = "./second.txt";
+
+
+function readFile(file) {
+	fs.readFile(file, "utf-8")
+		.then((text) => console.log(text))
+		.catch((err) => console.log(err));
+}
+readFile(firstFile);
+
+function createFile(file, data) {
+	fs.writeFile(file, data, (err) => {
+		console.log(err);
+	});
+}
+createFile(secondFile, "");
